c60fcb9d9fc43e35d018c7717a8092d60767ba5f
Alison Hill <apreshill@gmail.com>
2020-06-12 21:42:57 -0700

    update results docs

--- R/results.R
+++ R/results.R
@@ -1,10 +1,10 @@
 #' Each baker's fate, by episode
 #'
-#' Results for all bakers per episode across all GBBO series
+#' Results for all bakers per episode across all 10 GBBO series
 #'
-#' @format A data frame with 886 rows representing individual bakers per episode and 4 variables:
+#' @format A data frame with 1,136 rows representing individual bakers per episode and 4 variables:
 #' \describe{
-#'   \item{series}{an integer denoting UK series (1-8)}
+#'   \item{series}{an integer denoting UK series (1-10)}
 #'   \item{episode}{an integer denoting episode number within a series}
 #'   \item{baker}{a character string giving given or nickname only (note: see `bakers` for full baker names)}
 #'   \item{result}{a character string denoting if the baker was `IN`, `OUT`, `STAR BAKER`, or `SICK` for a given episode. For finale episodes, values are either `WINNER` or `RUNNER-UP`. If `NA`, baker did not appear in episode.}
