f8539095aa1b507007cbe72429dff05bc08623db
Alison Hill <apreshill@gmail.com>
2020-06-12 21:41:02 -0700

    update challenges docs

--- R/challenges.R
+++ R/challenges.R
@@ -1,10 +1,10 @@
 #' Each baker's signature, technical, and showstopper challenges, by episode
 #'
-#' Details about the three challenges (signature, technical, and showstopper) for each baker/episode across all GBBO series
+#' Details about the three challenges (signature, technical, and showstopper) for each baker/episode across all 10 GBBO series
 #'
 #' @format A data frame with 548 rows representing individual bakers per episode and 6 variables:
 #' \describe{
-#'   \item{series}{an integer denoting UK series (1-8)}
+#'   \item{series}{an integer denoting UK series (1-10)}
 #'   \item{episode}{an integer denoting episode number within a series}
 #'   \item{baker}{a character string giving given or nickname}
 #'   \item{signature}{a character string containing the bake for the signature challenge for that baker/episode}
