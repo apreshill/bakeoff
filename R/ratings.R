8b7fc0f7f5a14d38dacd9b6189d2fcc76daefa70
Alison Hill <apreshill@gmail.com>
2020-06-12 21:41:56 -0700

    update ratings docs

--- R/ratings.R
+++ R/ratings.R
@@ -1,10 +1,10 @@
 #' Each episode's ratings
 #'
-#' Ratings for all episodes across all GBBO series
+#' Ratings for all episodes across all 10 GBBO series
 #'
 #' @format A data frame with 74 rows representing individual episodes and 10 variables:
 #' \describe{
-#'   \item{series}{an integer denoting UK series (1-8)}
+#'   \item{series}{an integer denoting UK series (1-10)}
 #'   \item{episode}{an integer denoting episode number within a series}
 #'   \item{uk_airdate}{a date denoting original airdate of episode in the UK}
 #'   \item{viewers_7day}{number of viewers in millions within a 7-day window from airdate}
