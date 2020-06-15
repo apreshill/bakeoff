00e96c2db2ed71794d40997814462531d1f035fa
Alison Hill <apreshill@gmail.com>
2020-06-14 19:28:07 -0700

    add json

--- data-raw/04-episodes-wrangle.R
+++ data-raw/04-episodes-wrangle.R
@@ -1,7 +1,7 @@
-challenges <- readr::read_csv(here::here("data-raw", "challenges.csv"),
+episodes <- readr::read_csv(here::here("data-raw", "episodes.csv"),
                          col_types = cols(series = col_integer(),
                                           episode = col_integer(),
                                           technical = col_integer()))
 
 # end of exports
-usethis::use_data(challenges, overwrite = TRUE)
+usethis::use_data(episodes, overwrite = TRUE)
