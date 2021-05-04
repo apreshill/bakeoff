ee83354ab32df793fa949428baa00f2d30cd3892
Alison Presmanes Hill <alison@rstudio.com>
2021-05-03 20:40:21 -0700

    Dev pkgdown (#8)
    * first try
    
    * semi-happy
    
    * small tweak (#7)
    
    * futzing
    
    * redocument
    
    * update pkgdown site
    
    * rm bakeoff pkg doc file
    
    * correct varnames from devtools::check
    
    * update design
    
    * bslib variables
    
    * update css
    
    * active
    
    * hover vs active
    
    * rev bg
    
    * rebuild
    
    * more active
    
    * bump ref text down
    
    * rebuild
    
    Co-authored-by: Maëlle Salmon <maelle.salmon@yahoo.se>

--- R/seasons.R
+++ R/seasons.R
@@ -5,7 +5,7 @@
 #' @format A data frame with 8 rows representing individual series and 11 variables:
 #' \describe{
 #'   \item{series}{an integer denoting UK series (1-8)}
-#'   \item{episodes}{an integer denoting total number of episodes within series}
+#'   \item{episode}{an integer denoting total number of episodes within series}
 #'   \item{us_season}{an integer denoting US season (1-5)}
 #'   \item{us_airdate}{a date denoting original airdate of episode in the US, according to [pbs.org](http://www.pbs.org/food/shows/great-british-baking-show/)}
 #' }
