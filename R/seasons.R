bd7640b2017adea0d86ec66e8a8dee204448bb84
Richard Iannone <riannone@me.com>
2022-02-23 11:40:17 -0500

    Update seasons.R

--- R/seasons.R
+++ R/seasons.R
@@ -6,9 +6,9 @@
 #' @format A data frame with 8 rows representing individual series and 11
 #'   variables:
 #' \describe{
-#'   \item{series}{an integer denoting UK series (1-8)}
+#'   \item{series}{an integer denoting UK series (`1`-`8`)}
 #'   \item{episode}{an integer denoting total number of episodes within series}
-#'   \item{us_season}{an integer denoting US season (1-5)}
+#'   \item{us_season}{an integer denoting US season (`1`-`5`)}
 #'   \item{us_airdate}{a date denoting original airdate of episode in the US,
 #'   according to
 #'   [pbs.org](https://www.pbs.org/food/shows/great-british-baking-show/)}
